85968f8f40b247fba7f4471d9c3e13509a35ee81
MarcinRutkowskii <marcinrut90@gmail.com>
2018-01-13 09:42:07 +0100

    fdsf

---
+++ JavaScript ksiazka/js/string-with-quotes.js
@@ -0,0 +1,9 @@
+var title;
+var message;
+title = "Oferta specjalna w Molly's";
+message = '<a href=\"sale.html\">Rabat 25%!</a>';
+
+var elTitle = document.getElementById('title');
+elTitle.innerHTML = title;
+var elNote = document.getElementById('note');
+elNote.innerHTML = message;
