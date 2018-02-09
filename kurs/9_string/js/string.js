48e270f171734fa59c0268c3ceb22c54b8295ecb
MarcinRutkowskii <marcinrut90@gmail.com>
2018-02-09 19:03:28 +0100

    kurs

--- kurs/9_string/js/string.js
+++ kurs/9_string/js/string.js
@@ -1,1 +1,16 @@
-alert ('ops');
+var text = 'Kurs programowania - JS i PHP';
+console.log(text);
+console.log(text.length);  //29, rowniez spacje sie wliczaja
+
+var pierwszyZnak = text.charAt(0);
+console.log(pierwszyZnak); //K
+
+var ostatniZnak = text.charAt(text.length - 1);
+console.log(ostatniZnak); //P
+
+var kod = text.charCodeAt(0);
+console.log(kod);  //75 tablica ASCII
+
+//################## zamiana na duże litery #################
+
+var duza = text.toUpperCase;
