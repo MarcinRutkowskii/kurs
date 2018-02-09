9602c72ef2383619ce350666466e3ac3b54e01dd
MarcinRutkowskii <marcinrut90@gmail.com>
2018-02-09 19:47:32 +0100

    kurs

--- kurs/9_string/js/string.js
+++ kurs/9_string/js/string.js
@@ -13,4 +13,36 @@ console.log(kod);  //75 tablica ASCII
 
 //################## zamiana na duże litery #################
 
-var duza = text.toUpperCase;
+var duza = text.toUpperCase();
+console.log(duza);
+
+//################ zamiana na małe litery
+
+var mala = text.toLowerCase();
+console.log(mala);
+//zamien imie wprowadzone w formularzu na:
+//1 litera duza , pozostale litery male
+
+var elImie = document.getElementById('imie');
+var elPrzycisk = document.getElementById('przycisk');
+var elKomunikat = document.getElementById('komunikat');
+var elNazwisko = document.getElementById('nazwisko');
+var imie;
+var poprawneImie;
+var nazwisko;
+var poprawneNazwisko;
+
+function zamienImie(){
+    imie = elImie.value;
+    poprawneImie = imie.charAt(0).toUpperCase() + imie.slice(1).toLowerCase();
+    nazwisko = elNazwisko.value;
+    poprawneNazwisko = nazwisko.charAt(0).toUpperCase() + nazwisko.slice(1).toLowerCase();
+    elKomunikat.innerHTML = '<br>Twoje imie: ' + poprawneImie + '<br>Twoje nazwisko: ' + poprawneNazwisko;
+    elKomunikat.style.color = 'red';
+
+
+
+
+
+}
+elPrzycisk.addEventListener('click', zamienImie);
