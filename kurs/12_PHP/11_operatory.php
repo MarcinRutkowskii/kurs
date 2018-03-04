b1fdaa75b4fb2f0f3bba9fba2b643a03f0e154fd
MarcinRutkowskii <marcinrut90@gmail.com>
2018-03-04 14:01:38 +0100

    kurs

--- kurs/12_PHP/11_operatory.php
+++ kurs/12_PHP/11_operatory.php
@@ -106,4 +106,93 @@ $z = 7;
 
 $wynik = $x ?? $y ?? $z; //wyswietla pierwsza wartosc jaka jest
 echo $wynik;
+echo '<br>';
+//operatory łańcuchowe
+
+$tekst1 = 'Jan';
+$tekst2 = 'Kowal';
+echo $tekst1.' '.$tekst2.'<br>';
+echo $tekst1,'jan';
+echo '<br>';
+
+//operatory przypisania
+//=, +=, -=, *=, /=, %=, .=,
+//istnieja rowniez operatory bitowe
+
+$a = 5;
+echo gettype($a);//integer
+$a .= '';
+echo gettype ($a);
+$b = 10;
+echo gettype($b); //integer
+$a += $b; // $a = $a+$b;//15
+$b .= $a; // $b = 10 + 15 = 1015
+echo $b;
+echo gettype($b); //string
+echo'<br><br>';
+############################################
+//operator warunkowy
+
+$x = 5;
+$wynik = ($x > 2)?'x wieksze od 2': 'x nie jest wieksze od 2'; // ? oznacza ze cos sprawdzamy, : oznacza else, 3 operandowy operator warunkowy, lepiej to stosowac zamiast else if, bo tak robia lepsi programisci
+echo $wynik;
+echo '<br><br>';
+
+####################################
+//operatory rzutowania (konwersji typów)
+//int integer, float double real, bool boolean, string, array, object, unset
+$x = '2';
+echo gettype($x); //string
+echo'<br>';
+$x = (int)$x;
+echo gettype($x);//integer
+echo '<br>';
+$x = (double)$x;
+echo gettype($x);//double
+echo '<br>';
+$x = (unset)$x;
+echo gettype($x);//NULL
+echo '<br><br>';
+//priorytety operatorów 23 typy
+//&& ma wiekszy priorytet niz AND
+//|| ma wiekszy priorytet niz OR
+##################################################
+//konwersja typów jawna lunb niejawna
+
+$napis = 'Janusz';
+echo gettype($napis); //jawna zmiana
+
+$napis = 10;
+echo gettype($napis); //niejawna zmiana typow danych
+echo'<br><br>';
+
+//funkcje konwersji
+//intval(), floatval(), doubleval(), stringval(), boolval()
+
+$liczba = 2.5;
+//$liczba=(int)$liczba;
+$liczba = intval($liczba);
+echo gettype($liczba).' '.$liczba;
+echo '<br>';
+//settype()
+
+$liczba1 = 10.5;
+settype($liczba1,'int'); //settype nie wolno przypisywac do zmiennej bo wyswietli ze sie wykonala i wyswietli true
+//if(settype($liczba1,'int))
+//  echo 'poprawna konwersja';
+//echo get type($liczba1);
+
+//echo $liczba1.gettype($liczba1);
+echo gettype($liczba1);
+echo '<br>';
+$liczba2 = 5;
+settype($liczba2,'double');
+echo gettype($liczba2);
+echo'<br>';
+
+$liczba3 = 0;
+settype($liczba3,'bool');
+echo $liczba3; //false, taka zasada,ze przy konwersji wartosci nie przypisanej lub o wartosci 0 wyjdzie false przy tej funkcji
+
+
 ?>
